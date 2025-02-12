54fc63f5ee7c92029364f8ea5e2a40427f6c14fc
nicolo_canevari <nico.canevari@outlook.it>
2025-02-12 14:28:52 +0100

    [FEAT] html header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+export default function Header() {
+
+    return (
+
+        <header>
+
+            <figure>
+                <img src="./public/dc-logo.png" alt="logo" />
+            </figure>
+
+            <nav>
+
+                <ul>
+
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+
+                </ul>
+
+            </nav>
+
+        </header>
+
+    );
+
+}
