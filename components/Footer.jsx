9c39be2f1654f562f33a8312b1121681894a82c9
nicolo_canevari <nico.canevari@outlook.it>
2025-02-12 18:11:35 +0100

    [FEAT] bozza footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,15 +4,82 @@ export default function Footer() {
 
         <footer>
 
-            <section className="link">
+            <section id="fcont1">
 
-                <div></div>
-                <div></div>
-                <div></div>
+                <div id="link">
+
+                    <div className="column">
+
+                        <h2>DC COMICS</h2>
+                        <ul>
+
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+
+                        </ul>
+
+                        <h2>SHOP</h2>
+                        <ul>
+
+                            <li></li>
+                            <li></li>
+
+                        </ul>
+
+                    </div>
+
+                    <div className="column">
+
+                        <h2>DC</h2>
+                        <ul>
+
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+
+                        </ul>
+
+                    </div>
+
+                    <div className="column">
+
+                        <h2>SITES</h2>
+                        <ul>
+
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+
+                        </ul>
+
+                    </div>
+
+                </div>
+
+                <div id="f_logo">
+
+                    <img src="./public/img/dc-logo-bg.png" alt="" />
+
+                </div>
 
             </section>
 
-            <section className="social">
+            <section id="fcont2">
 
                 <div className="signup"><button><h3>SING-UP NOW!</h3></button></div>
 
