886ff1519e19b24774f13bc3e5ff9d8cb8b36a37
nicolo_canevari <nico.canevari@outlook.it>
2025-02-12 19:22:49 +0100

    [FEAT] footer parte finale bozza css

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,21 +13,21 @@ export default function Footer() {
                         <h2>DC COMICS</h2>
                         <ul>
 
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Comics</a></li>
+                            <li><a href="">Movies</a></li>
+                            <li><a href="">TV</a></li>
+                            <li><a href="">Games</a></li>
+                            <li><a href="">Videos</a></li>
+                            <li><a href="">News</a></li>
 
                         </ul>
 
                         <h2>SHOP</h2>
                         <ul>
 
-                            <li></li>
-                            <li></li>
+                            <li><a href="">Shop DC</a></li>
+                            <li><a href="">Shop DC Collectibles</a></li>
 
                         </ul>
 
@@ -38,17 +38,17 @@ export default function Footer() {
                         <h2>DC</h2>
                         <ul>
 
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li><a href="">Terms of Use</a></li>
+                            <li><a href="">Privacy policy (New)</a></li>
+                            <li><a href="">Ad Choises</a></li>
+                            <li><a href="">Advertaising</a></li>
+                            <li><a href="">Jobs</a></li>
+                            <li><a href="">Subscriptions</a></li>
+                            <li><a href="">Talent Workshop</a></li>
+                            <li><a href="">CPSC Certificates</a></li>
+                            <li><a href="">Ratings</a></li>
+                            <li><a href="">Shop Help</a></li>
+                            <li><a href="">Contact Us</a></li>
 
                         </ul>
 
@@ -59,11 +59,11 @@ export default function Footer() {
                         <h2>SITES</h2>
                         <ul>
 
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li><a href="">DC</a></li>
+                            <li><a href="">MAD Magazine</a></li>
+                            <li><a href="">DC kids</a></li>
+                            <li><a href="">DC universe</a></li>
+                            <li><a href="">DC Power Visa</a></li>
 
                         </ul>
 
@@ -81,16 +81,25 @@ export default function Footer() {
 
             <section id="fcont2">
 
-                <div className="signup"><button><h3>SING-UP NOW!</h3></button></div>
+                <div className="signup">
+
+                    <button>
+
+                        <h3>SING-UP NOW!</h3>
+
+                    </button>
+
+                </div>
 
                 <div className="icons">
 
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                    <h3>FOLLOW US</h3>
+
+                    <img src="./public/img/footer-facebook.png" alt="" />
+                    <img src="./public/img/footer-twitter.png" alt="" />
+                    <img src="./public/img/footer-youtube.png" alt="" />
+                    <img src="./public/img/footer-pinterest.png" alt="" />
+                    <img src="./public/img/footer-periscope.png" alt="" />
 
                 </div>
 
