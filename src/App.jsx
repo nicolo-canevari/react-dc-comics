2db228844b5831aad77ccf8a076a134e7d610188
nicolo_canevari <nico.canevari@outlook.it>
2025-02-13 16:06:50 +0100

    [FEAT] array dei link del footer in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,43 @@ function App() {
 
   ];
 
+  // Array di link per il footer
+  const footerLinks = {
+    dcComics: [
+      { name: "Characters", href: "#" },
+      { name: "Comics", href: "#" },
+      { name: "Movies", href: "#" },
+      { name: "TV", href: "#" },
+      { name: "Games", href: "#" },
+      { name: "Videos", href: "#" },
+      { name: "News", href: "#" }
+    ],
+    shop: [
+      { name: "Shop DC", href: "#" },
+      { name: "Shop DC Collectibles", href: "#" }
+    ],
+    dc: [
+      { name: "Terms of Use", href: "#" },
+      { name: "Privacy policy", href: "#" },
+      { name: "Ad Choices", href: "#" },
+      { name: "Advertising", href: "#" },
+      { name: "Jobs", href: "#" },
+      { name: "Subscriptions", href: "#" },
+      { name: "Talent Workshop", href: "#" },
+      { name: "CPSC Certificates", href: "#" },
+      { name: "Ratings", href: "#" },
+      { name: "Shop Help", href: "#" },
+      { name: "Contact Us", href: "#" }
+    ],
+    sites: [
+      { name: "DC", href: "#" },
+      { name: "MAD Magazine", href: "#" },
+      { name: "DC Kids", href: "#" },
+      { name: "DC Universe", href: "#" },
+      { name: "DC Power Visa", href: "#" }
+    ]
+  };
+
   return (
 
     <>
@@ -27,7 +64,7 @@ function App() {
       {/* Passo l'array di link come prop */}
       <Header links={navBarLinks} />
       <Main />
-      <Footer />
+      <Footer links={footerLinks} />
 
     </>
 
