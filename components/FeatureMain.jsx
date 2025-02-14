ae7c80f39cb1f80fb9ee7c936257feed49969c21
nicolo_canevari <nico.canevari@outlook.it>
2025-02-14 18:16:11 +0100

    [FIX] problemi con una classe che entrava in conflitto con .card

--- components/FeatureMain.jsx
+++ components/FeatureMain.jsx
@@ -7,27 +7,27 @@ export default function FeatureMain() {
 
             <section id="feature">
 
-                <div className="card">
+                <div className="card_icons">
                     <img src="./public/img/buy-comics-digital-comics.png" alt="" />
                     <span>DIGITAL COMICS</span>
                 </div>
 
-                <div className="card">
+                <div className="card_icons">
                     <img src="./public/img/buy-comics-merchandise.png" alt="" />
                     <span>DC MERCHANDISE</span>
                 </div>
 
-                <div className="card">
+                <div className="card_icons">
                     <img src="./public/img/buy-comics-subscriptions.png" alt="" />
                     <span>SUBSCRIPTION</span>
                 </div>
 
-                <div className="card">
+                <div className="card_icons">
                     <img src="./public/img/buy-comics-shop-locator.png" alt="" />
                     <span>COMIC SHOP LOCATOR</span>
                 </div>
 
-                <div className="card">
+                <div className="card_icons">
                     <img id="svg" src="./public/img/buy-dc-power-visa.svg" alt="" />
                     <span>DC POWER VISA</span>
                 </div>
